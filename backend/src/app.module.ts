d9842429f595d872a723d46545202f5f5e7ff1f1
Usama Ahmed <maxpaynestory@gmail.com>
2021-08-02 17:09:30 +0500

    backend connected with mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?authSource=admin`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
